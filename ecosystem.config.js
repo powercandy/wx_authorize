d44be3b83c69d6b747264e5963de6689ab7cf6c4
胡江 <17600112740@163.com>
2019-09-19 21:15:09 +0800

    fix: config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         script: 'bin/www',
         // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
         cwd: './',
-        watch: ['app', 'bin', 'config', 'app.js'],
+        watch: ['app/*', 'bin/*', 'config/*', 'app.js'],
         watch_delay: 1000,
         watch_options: {
             followSymlinks: false,
