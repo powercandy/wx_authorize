e39a304ae5c0928dfd5757937a2bd5fca505680c
胡江 <17600112740@163.com>
2019-09-20 15:05:44 +0800

    fix: log.out signature

--- app/middleware/wxSignature.js
+++ app/middleware/wxSignature.js
@@ -30,7 +30,7 @@ exports.getSignature = (req, res, next) => {
     log.out('timestamp --- ' + timestamp);
     log.out('ticket --- ' + ticket);
     log.out('noncestr --- ' + noncestr);
-    log.out('url --- ' + url);
+    log.out(req.query);
     log.out('str --- ' + str);
     log.out('signature --- ' + signature);
     req.result = {
