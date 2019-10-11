32c71d9737a048ef8059ceb5cb256605547369ce
胡江 <17600112740@163.com>
2019-10-11 10:31:58 +0800

    切换配置文件

--- config/wxConfig.js
+++ config/wxConfig.js
@@ -1,14 +1,14 @@
+module.exports = {
+    appId: 'wx3816e88e3c0c6c28',
+    appSecret: '8a1669170955ab2c68e5a18c748dc081',
+    // host: 'https://www.sneptune.cn',
+    host: '106.13.69.229',
+    token: 'sneptune'
+};
 // module.exports = {
-//     appId: 'wx3816e88e3c0c6c28',
-//     appSecret: '8a1669170955ab2c68e5a18c748dc081',
+//     appId: 'wxcd0b833018b5cdc1',
+//     appSecret: '0088151d91a5adf1091b8798ac5fb4c5',
 //     // host: 'https://www.sneptune.cn',
 //     host: '106.13.69.229',
 //     token: 'sneptune'
 // }
-module.exports = {
-    appId: 'wxcd0b833018b5cdc1',
-    appSecret: '0088151d91a5adf1091b8798ac5fb4c5',
-    // host: 'https://www.sneptune.cn',
-    host: '106.13.69.229',
-    token: 'sneptune'
-}
